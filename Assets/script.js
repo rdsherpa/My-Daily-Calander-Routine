fe412bcf6dd511fa2b8356d6f4dc4e9138f4f02e
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2022-09-19 23:02:58 -0700

    This my daily calander application

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function (){
+  // listen for save button clicks
+  $('.saveBtn').on('click', function () {
+  });
+
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      }
+      else {
+        $(this).reomveClass('past');
+        $(this).reomveClass('present');
+        $(this).reomveClass('future');
+    }
+  });
+}
+
+hourUpdater();
+
+// set up interval to check if current time needs to be updated
+var interval = setInterval(hourUpdater, 15000);
+
+// load any saved data from localStorage
+$('hour-9 .description').val(localStorage.getItem('hour-9'));
+$('hour-10 .description').val(localStorage.getItem('hour-10'));
+$('hour-11 .description').val(localStorage.getItem('hour-11'));
+$('hour-12 .description').val(localStorage.getItem('hour-12'));
+$('hour-1 .description').val(localStorage.getItem('hour-1'));
+$('hour-2 .description').val(localStorage.getItem('hour-2'));
+$('hour-3 .description').val(localStorage.getItem('hour-3'));
+$('hour-4 .description').val(localStorage.getItem('hour-4'));
+$('hour-5 .description').val(localStorage.getItem('hour-5'));
+
+// display current day on page
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
+});
---
+++ README.md
@@ -0,0 +1,21 @@
+# <My-Daily-Calander-Routine>
+
+## Description
+
+- What was your motivation?
+My days are always busy and it is difficult for me to keep everything in mind. That is the reason I use calander application to make my life easier. My main motivation for building this project was to learn how to built application like this. How can I make it more convient to all users. 
+
+- Why did you build this project? 
+I wanted to learn more in depth about how application like this work and I also wanted what I learned from this class. I want to apply all the skill that I have learned and make it more convient for the users so that users can easily input and remind about their daily dask. 
+
+- What problem does it solve?
+It helps the doubts whether I will be able to build this application. I am more able to learn about third party APIs and I don't only have to rely on CSS style sheet. JQuery, bootstraps helps me to save time and think smart. 
+
+- What did you learn?
+I leard to research more and use JQuery, Bootstraps, Moment.JS which can be used as third Party APIs. I also learnd combining all these will help you to built and awesome application. 
+
+# <Screen-Shot-of-My-Daily-Calander-Routine>
+
+![alt text](/Screen%20Shot%202022-09-19%20at%2010.53.52%20PM.png)
+![alt text](/Screen%20Shot%202022-09-19%20at%2010.54.38%20PM.png)
+
